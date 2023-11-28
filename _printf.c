cd4ca57f8a839a9fd6d18155a1746fbf0de1e5a0
root <valibm>
2023-11-28 02:45:48 -0800

    Added initial version of  _printf.c

---
+++ _printf.c
@@ -0,0 +1,9 @@
+/**
+ *_printf - Is a function that produces output according to a format.
+ * @format: Is a character string.
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	
+}
