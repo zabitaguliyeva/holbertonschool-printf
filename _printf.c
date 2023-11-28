71ccf578dc956f5f11f73e74aa8e2b47a2cc4a2c
zabitaguliyeva <7644@holbertonstudents.com.com>
2023-11-28 12:32:56 -0800

    d

--- _printf.c
+++ _printf.c
@@ -2,47 +2,58 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int _printf(const char *format, ...) {
-	    int i, len = 0;
-	        char ch;
-		    char *str;
-		        va_list ap;
+int _printf(const char *format, ...)
+{
+	int i, len = 0;
+	char ch;
+	char *str;
+	va_list ap;
 
-			    if (!format)
-				            exit(60);
+	if (!format)
+		exit(60);
 
-			        va_start(ap, format);
-				    for (i = 0; format[i] != '\0'; i++) {
-					            if (format[i] == '%') {
-							                i++;
-									            if (format[i] == 'c') {
-											                    ch = (char)va_arg(ap, int);
-													                    len += write(1, &ch, 1);
-															                } else if (format[i] == 's') {
-																		                str = va_arg(ap, char *);
-																				                if (str == NULL)
-																							                    str = "(null)";
-																						                int j = 0;
-																								                while (str[j] != '\0') {
-																											                    len += write(1, &str[j], 1);
-																													                        j++;
-																																                }
-																										            } else if (format[i] == '%') {
-																												                    ch = '%';
-																														                    len += write(1, &ch, 1);
-																																                } else {
-																																			                ch = '%';
-																																					                len += write(1, &ch, 1);
-																																							                ch = format[i];
-																																									                len += write(1, &ch, 1);
-																																											            }
-										            } else {
-												                ch = format[i];
-														            len += write(1, &ch, 1);
-															            }
-						        }
-
-				        va_end(ap);
-					    return len;
+	va_start(ap, format);
+	for (i = 0; format[i] !='\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				ch = (char)va_arg(ap, int);
+				len += write(1, &ch, 1);
+			}
+			else if (format [i] == 's')
+			{
+				str = va_arg(ap, char *);
+				if (str == NULL)
+					str = "(null)";
+				int j = 0;
+				while (str[j] != '\0')
+				{
+					len += write(1, &str[j], 1);
+					j++;
+				}
+			}
+			else if (format [i] == '%')
+			{
+				ch = '%';
+				len += write(1, &ch, 1);
+			}
+			else
+			{
+				ch = '%';
+				len += write(1, &ch, 1);
+				ch = format[i];
+				len += write(1, &ch, 1);
+			}
+		}
+		else
+		{
+			ch =format[i];
+			len += write(1, &ch, 1);
+		}
+	}
+	va_end(ap);
+	return len;
 }
-
