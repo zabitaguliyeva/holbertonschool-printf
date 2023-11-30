c94ddd0f33cc92b2553f0052bd5b66a27630a04c
root <valibm>
2023-11-30 02:15:42 -0800

    Task 1

---
+++ int_funcs.c
@@ -0,0 +1,75 @@
+#include "main.h"
+#include "unistd.h"
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+
+	return (n);
+}
+
+char *_reverse(char *str, int n)
+{
+	int i, temp;
+
+	n--;
+	for (i = 0; i <= n; i++, n--)
+	{
+		temp = str[i];
+		str[i] = str[n];
+		str[n] = temp;
+	}
+
+	return (str);
+}
+
+int print_int_string(char *s, int *len)
+{
+	int j = 0;
+
+	while (s[j] != '\0')
+	{
+		(*len) += write(1, &s[j], 1);
+		j++;
+	}
+
+	return (*len);
+}
+
+char *_itoa(int value)
+{
+	char buffer[1024];
+	int n, i;
+
+	n = _abs(value);
+
+	i = 0;
+	while (n)
+	{
+		int r = n % 10;
+
+		buffer[i] = 48 + r;
+		i++;
+		n = n / 10;
+	}
+
+	if (i == 0)
+	{
+		buffer[i] = '0';
+		i++;
+	}
+
+	if (value < 0)
+	{
+		buffer[i] = '-';
+		i++;
+	}
+
+	buffer[i] = '\0';
+
+	return (_reverse(buffer, i));
+}
--- main.h
+++ main.h
@@ -1,7 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
+
 int _printf(const char *format, ...);
 int print_char(va_list ap);
 int print_string(va_list ap, int*);
+void print_int(va_list value, int *len);
+int print_int_string(char *s, int *len);
+char *_reverse(char *s, int n);
+char *_itoa(int value);
+int _abs(int n);
+
 #endif
